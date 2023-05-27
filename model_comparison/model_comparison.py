2696a47c66b5de209c3de7e08f376d226727548f
Deborah Marcandella <deborah.marcandella@regionalwerke.ch>
2023-05-27 21:20:06 +0200

    add def compare_metrics_over_all_models():

--- model_comparison/model_comparison.py
+++ model_comparison/model_comparison.py
@@ -40,177 +40,135 @@ def compare_metrics_each_model():
 def compare_metrics_over_all_models():
     yolov8s_1632_100_8_811 = pd.read_csv(os.path.join(HOME, 'yolov8s_1632_100_8_811/results.csv'))
     yolov8s_1632_100_8_811.columns = yolov8s_1632_100_8_811.columns.str.lstrip()
+    yolov8s_1632_100_8_811['model'] = 's_1632_100_8_811'
 
     yolov8s_1632_100_16_811 = pd.read_csv(os.path.join(HOME, 'yolov8s_1632_100_16_811/results.csv'))
     yolov8s_1632_100_16_811.columns = yolov8s_1632_100_16_811.columns.str.lstrip()
+    yolov8s_1632_100_16_811['model'] = 's_1632_100_16_811'
 
     yolov8m_1632_100_16_811 = pd.read_csv(os.path.join(HOME, 'yolov8m_1632_100_16_811/results.csv'))
     yolov8m_1632_100_16_811.columns = yolov8m_1632_100_16_811.columns.str.lstrip()
+    yolov8m_1632_100_16_811['model'] = 'm_1632_100_16_811'
 
     yolov8m_3408_150_16_811 = pd.read_csv(os.path.join(HOME, 'yolov8m_3408_150_16_811/results.csv'))
     yolov8m_3408_150_16_811.columns = yolov8m_3408_150_16_811.columns.str.lstrip()
+    yolov8m_3408_150_16_811['model'] = 'm_3408_150_16_811'
 
     yolov8m_4524_150_16_811 = pd.read_csv(os.path.join(HOME, 'yolov8m_4524_150_16_811/results.csv'))
     yolov8m_4524_150_16_811.columns = yolov8m_4524_150_16_811.columns.str.lstrip()
+    yolov8m_4524_150_16_811['model'] = 'm_4524_150_16_811'
 
     yolov8m_6040_150_16_811 = pd.read_csv(os.path.join(HOME, 'yolov8m_6040_150_16_811/results.csv'))
     yolov8m_6040_150_16_811.columns = yolov8m_6040_150_16_811.columns.str.lstrip()
+    yolov8m_6040_150_16_811['model'] = 'm_6040_150_16_811'
 
     yolov8m_6040_150_16_721 = pd.read_csv(os.path.join(HOME, 'yolov8m_6040_150_16_721/results.csv'))
     yolov8m_6040_150_16_721.columns = yolov8m_6040_150_16_721.columns.str.lstrip()
+    yolov8m_6040_150_16_721['model'] = 'm_6040_150_16_721'
 
-    yolov8s_1632_100_8_811['metrics/mAP50(B)'].plot(label='s_1632_100_8_811', color='deepskyblue')
-    yolov8s_1632_100_16_811['metrics/mAP50(B)'].plot(label='s_1632_100_16_811', color='blue')
-    yolov8m_1632_100_16_811['metrics/mAP50(B)'].plot(label='m_1632_100_16_811', color='cyan')
-    yolov8m_3408_150_16_811['metrics/mAP50(B)'].plot(label='m_3408_150_16_811', color='orange')
-    yolov8m_4524_150_16_811['metrics/mAP50(B)'].plot(label='m_4524_150_16_811', color='red')
-    yolov8m_6040_150_16_811['metrics/mAP50(B)'].plot(label='m_6040_150_16_811', color='magenta')
-    yolov8m_6040_150_16_721['metrics/mAP50(B)'].plot(label='m_6040_150_16_721', color='darkviolet')
+    df_model_comp = pd.concat([yolov8s_1632_100_8_811, yolov8s_1632_100_16_811, yolov8m_1632_100_16_811,
+                               yolov8m_3408_150_16_811, yolov8m_4524_150_16_811, yolov8m_6040_150_16_811,
+                               yolov8m_6040_150_16_721])
 
-    # adding title to the plot
+    # Create Data Frame for train and val comparison
+    df_model_comp_train = df_model_comp.loc[:, ['epoch', 'model', 'train/box_loss', 'train/cls_loss', 'train/dfl_loss']]
+    df_model_comp_train['task'] = 'train'
+    df_model_comp_train.rename(columns={'train/box_loss': 'box_loss', 'train/cls_loss': 'cls_loss',
+                                        'train/dfl_loss': 'dfl_loss'}, inplace=True)
+
+    df_model_comp_val = df_model_comp.loc[:, ['epoch', 'model', 'val/box_loss', 'val/cls_loss', 'val/dfl_loss']]
+    df_model_comp_val['task'] = 'val'
+    df_model_comp_val.rename(
+        columns={'val/box_loss': 'box_loss', 'val/cls_loss': 'cls_loss', 'val/dfl_loss': 'dfl_loss'},
+        inplace=True)
+
+    df_model_comp_train_val = pd.concat([df_model_comp_train, df_model_comp_val])
+
+    # Comparison mAP50
+    sns.set_style('whitegrid')
+    ax = sns.lineplot(data=df_model_comp, x="epoch", y="metrics/mAP50(B)", hue="model", lw=1)
+    ax.set_yticks(np.arange(0, 1.1, 0.1))
+    ax.set_xticklabels(ax.get_xticklabels(), size=8)
+    ax.set_yticklabels(ax.get_yticklabels(), size=8)
+    ax.set(xlabel='Epoche', ylabel='mAP50')
+    plt.legend(loc='lower right', borderaxespad=0, fontsize=8)
     plt.title('Vergleich mAP50')
-
-    # adding Label to the x-axis
-    plt.xlabel('epoch')
-
-    # adding legend to the curve
-    plt.legend()
+    plt.savefig(r"D:\MAS_DataScience\Test\training_map50_epoch.png", dpi=300)
     plt.show()
     plt.cla()
 
-    yolov8s_1632_100_8_811['metrics/mAP50-95(B)'].plot(label='s_1632_100_8_811', color='deepskyblue')
-    yolov8s_1632_100_16_811['metrics/mAP50-95(B)'].plot(label='s_1632_100_16_811', color='blue')
-    yolov8m_1632_100_16_811['metrics/mAP50-95(B)'].plot(label='m_1632_100_16_811', color='cyan')
-    yolov8m_3408_150_16_811['metrics/mAP50-95(B)'].plot(label='m_3408_150_16_811', color='orange')
-    yolov8m_4524_150_16_811['metrics/mAP50-95(B)'].plot(label='m_4524_150_16_811', color='red')
-    yolov8m_6040_150_16_811['metrics/mAP50-95(B)'].plot(label='m_6040_150_16_811', color='magenta')
-    yolov8m_6040_150_16_721['metrics/mAP50-95(B)'].plot(label='m_6040_150_16_721', color='darkviolet')
-
-    # adding title to the plot
+    # Comparison mAP50-95
+    sns.set_style('whitegrid')
+    ax = sns.lineplot(data=df_model_comp, x="epoch", y="metrics/mAP50-95(B)", hue="model", lw=1)
+    ax.set_yticks(np.arange(0, 1.1, 0.1))
+    ax.set_xticklabels(ax.get_xticklabels(), size=8)
+    ax.set_yticklabels(ax.get_yticklabels(), size=8)
+    ax.set(xlabel='Epoche', ylabel='mAP50')
+    plt.legend(loc='lower right', borderaxespad=0, fontsize=8)
     plt.title('Vergleich mAP50-95')
-
-    # adding Label to the x-axis
-    plt.xlabel('Epoche')
-
-    # adding legend to the curve
-    plt.legend()
+    plt.savefig(r"D:\MAS_DataScience\Test\training_map50-95_epoch.png", dpi=300)
     plt.show()
     plt.cla()
 
-    yolov8s_1632_100_8_811['metrics/recall(B)'].plot(label='s_1632_100_8_811', color='deepskyblue')
-    yolov8s_1632_100_16_811['metrics/recall(B)'].plot(label='s_1632_100_16_811', color='blue')
-    yolov8m_1632_100_16_811['metrics/recall(B)'].plot(label='m_1632_100_16_811', color='cyan')
-    yolov8m_3408_150_16_811['metrics/recall(B)'].plot(label='m_3408_150_16_811', color='orange')
-    yolov8m_4524_150_16_811['metrics/recall(B)'].plot(label='m_4524_150_16_811', color='red')
-    yolov8m_6040_150_16_811['metrics/recall(B)'].plot(label='m_6040_150_16_811', color='magenta')
-    yolov8m_6040_150_16_721['metrics/recall(B)'].plot(label='m_6040_150_16_721', color='darkviolet')
-
-    # adding title to the plot
+    # Comparison Recall
+    sns.set_style('whitegrid')
+    ax = sns.lineplot(data=df_model_comp, x="epoch", y="metrics/recall(B)", hue="model", lw=1)
+    ax.set_yticks(np.arange(0, 1.1, 0.1))
+    ax.set_xticklabels(ax.get_xticklabels(), size=8)
+    ax.set_yticklabels(ax.get_yticklabels(), size=8)
+    ax.set(xlabel='Epoche', ylabel='mAP50')
+    plt.legend(loc='lower right', borderaxespad=0, fontsize=8)
     plt.title('Vergleich Recall')
-
-    # adding Label to the x-axis
-    plt.xlabel('Epoche')
-
-    # adding legend to the curve
-    plt.legend()
+    plt.savefig(r"D:\MAS_DataScience\Test\training_recall_epoch.png", dpi=300)
     plt.show()
     plt.cla()
 
-    yolov8s_1632_100_8_811['metrics/precision(B)'].plot(label='s_1632_100_8_811', color='deepskyblue')
-    yolov8s_1632_100_16_811['metrics/precision(B)'].plot(label='s_1632_100_16_811', color='blue')
-    yolov8m_1632_100_16_811['metrics/precision(B)'].plot(label='m_1632_100_16_811', color='cyan')
-    yolov8m_3408_150_16_811['metrics/precision(B)'].plot(label='m_3408_150_16_811', color='orange')
-    yolov8m_4524_150_16_811['metrics/precision(B)'].plot(label='m_4524_150_16_811', color='red')
-    yolov8m_6040_150_16_811['metrics/precision(B)'].plot(label='m_6040_150_16_811', color='magenta')
-    yolov8m_6040_150_16_721['metrics/precision(B)'].plot(label='m_6040_150_16_721', color='darkviolet')
-
-    # adding title to the plot
+    # Comparison Precision
+    sns.set_style('whitegrid')
+    ax = sns.lineplot(data=df_model_comp, x="epoch", y="metrics/precision(B)", hue="model", lw=1)
+    ax.set_yticks(np.arange(0, 1.1, 0.1))
+    ax.set_xticklabels(ax.get_xticklabels(), size=8)
+    ax.set_yticklabels(ax.get_yticklabels(), size=8)
+    ax.set(xlabel='Epoche', ylabel='mAP50')
+    plt.legend(loc='lower right', borderaxespad=0, fontsize=8)
     plt.title('Vergleich Precision')
-
-    # adding Label to the x-axis
-    plt.xlabel('Epoche')
-
-    # adding legend to the curve
-    plt.legend()
+    plt.savefig(r"D:\MAS_DataScience\Test\training_precision_epoch.png", dpi=300)
     plt.show()
     plt.cla()
 
-    yolov8s_1632_100_8_811['val/dfl_loss'].plot(label='s_1632_100_8_811', color='deepskyblue')
-    yolov8s_1632_100_16_811['val/dfl_loss'].plot(label='s_1632_100_16_811', color='blue')
-    yolov8m_1632_100_16_811['val/dfl_loss'].plot(label='m_1632_100_16_811', color='cyan')
-    yolov8m_3408_150_16_811['val/dfl_loss'].plot(label='m_3408_150_16_811', color='orange')
-    yolov8m_4524_150_16_811['val/dfl_loss'].plot(label='m_4524_150_16_811', color='red')
-    yolov8m_6040_150_16_811['val/dfl_loss'].plot(label='m_6040_150_16_811', color='magenta')
-    yolov8m_6040_150_16_721['val/dfl_loss'].plot(label='m_6040_150_16_721', color='darkviolet')
-    yolov8s_1632_100_8_811['train/dfl_loss'].plot(label='s_1632_100_8_811', color='deepskyblue', linestyle='dashed')
-    yolov8s_1632_100_16_811['train/dfl_loss'].plot(label='s_1632_100_16_811', color='blue', linestyle='dashed')
-    yolov8m_1632_100_16_811['train/dfl_loss'].plot(label='m_1632_100_16_811', color='cyan', linestyle='dashed')
-    yolov8m_3408_150_16_811['train/dfl_loss'].plot(label='m_3408_150_16_811', color='orange', linestyle='dashed')
-    yolov8m_4524_150_16_811['train/dfl_loss'].plot(label='m_4524_150_16_811', color='red', linestyle='dashed')
-    yolov8m_6040_150_16_811['train/dfl_loss'].plot(label='m_6040_150_16_811', color='magenta', linestyle='dashed')
-    yolov8m_6040_150_16_721['train/dfl_loss'].plot(label='m_6040_150_16_721', color='darkviolet', linestyle='dashed')
-
-
-    # adding title to the plot
-    plt.title('Vergleich DFL-Loss')
-
-    # adding Label to the x-axis
-    plt.xlabel('Epoche')
-
-    # adding legend to the curve
-    plt.legend()
-    plt.show()
-    plt.cla()
-
-    yolov8s_1632_100_8_811['val/cls_loss'].plot(label='s_1632_100_8_811', color='deepskyblue')
-    yolov8s_1632_100_16_811['val/cls_loss'].plot(label='s_1632_100_16_811', color='blue')
-    yolov8m_1632_100_16_811['val/cls_loss'].plot(label='m_1632_100_16_811', color='cyan')
-    yolov8m_3408_150_16_811['val/cls_loss'].plot(label='m_3408_150_16_811', color='orange')
-    yolov8m_4524_150_16_811['val/cls_loss'].plot(label='m_4524_150_16_811', color='red')
-    yolov8m_6040_150_16_811['val/cls_loss'].plot(label='m_6040_150_16_811', color='magenta')
-    yolov8m_6040_150_16_721['val/cls_loss'].plot(label='m_6040_150_16_721', color='darkviolet')
-    yolov8s_1632_100_8_811['train/cls_loss'].plot(label='s_1632_100_8_811', color='deepskyblue', linestyle='dashed')
-    yolov8s_1632_100_16_811['train/cls_loss'].plot(label='s_1632_100_16_811', color='blue', linestyle='dashed')
-    yolov8m_1632_100_16_811['train/cls_loss'].plot(label='m_1632_100_16_811', color='cyan', linestyle='dashed')
-    yolov8m_3408_150_16_811['train/cls_loss'].plot(label='m_3408_150_16_811', color='orange', linestyle='dashed')
-    yolov8m_4524_150_16_811['train/cls_loss'].plot(label='m_4524_150_16_811', color='red', linestyle='dashed')
-    yolov8m_6040_150_16_811['train/cls_loss'].plot(label='m_6040_150_16_811', color='magenta', linestyle='dashed')
-    yolov8m_6040_150_16_721['train/cls_loss'].plot(label='m_6040_150_16_721', color='darkviolet', linestyle='dashed')
-
-    # adding title to the plot
-    plt.title('Vergleich CLS-Loss')
-
-    # adding Label to the x-axis
-    plt.xlabel('Epoche')
-
-    # adding legend to the curve
-    plt.legend()
-    plt.show()
-    plt.cla()
-
-    yolov8s_1632_100_8_811['val/box_loss'].plot(label='s_1632_100_8_811', color='deepskyblue')
-    yolov8s_1632_100_16_811['val/box_loss'].plot(label='s_1632_100_16_811', color='blue')
-    yolov8m_1632_100_16_811['val/box_loss'].plot(label='m_1632_100_16_811', color='cyan')
-    yolov8m_3408_150_16_811['val/box_loss'].plot(label='m_3408_150_16_811', color='orange')
-    yolov8m_4524_150_16_811['val/box_loss'].plot(label='m_4524_150_16_811', color='red')
-    yolov8m_6040_150_16_811['val/box_loss'].plot(label='m_6040_150_16_811', color='magenta')
-    yolov8m_6040_150_16_721['val/box_loss'].plot(label='m_6040_150_16_721', color='darkviolet')
-    yolov8s_1632_100_8_811['train/box_loss'].plot(label='s_1632_100_8_811', color='deepskyblue', linestyle='dashed')
-    yolov8s_1632_100_16_811['train/box_loss'].plot(label='s_1632_100_16_811', color='blue', linestyle='dashed')
-    yolov8m_1632_100_16_811['train/box_loss'].plot(label='m_1632_100_16_811', color='cyan', linestyle='dashed')
-    yolov8m_3408_150_16_811['train/box_loss'].plot(label='m_3408_150_16_811', color='orange', linestyle='dashed')
-    yolov8m_4524_150_16_811['train/box_loss'].plot(label='m_4524_150_16_811', color='red', linestyle='dashed')
-    yolov8m_6040_150_16_811['train/box_loss'].plot(label='m_6040_150_16_811', color='magenta', linestyle='dashed')
-    yolov8m_6040_150_16_721['train/box_loss'].plot(label='m_6040_150_16_721', color='darkviolet', linestyle='dashed')
-
-    # adding title to the plot
+    # Comparison Box-Loss
+    sns.set_style('whitegrid')
+    ax = sns.lineplot(data=df_model_comp_train_val, x="epoch", y="box_loss", hue="model", style='task', lw=1)
+    ax.set_xticklabels(ax.get_xticklabels(), size=8)
+    ax.set_yticklabels(ax.get_yticklabels(), size=8)
+    ax.set(xlabel='Epoche', ylabel='box loss')
+    plt.legend(loc='upper right', borderaxespad=0, fontsize=8)
     plt.title('Vergleich Box-Loss')
+    plt.savefig(r"D:\MAS_DataScience\Test\training_box_loss_epoch.png", dpi=300)
+    plt.show()
+    plt.cla()
 
-    # adding Label to the x-axis
-    plt.xlabel('Epoche')
+    # Comparison Class-Loss
+    sns.set_style('whitegrid')
+    ax = sns.lineplot(data=df_model_comp_train_val, x="epoch", y="cls_loss", hue="model", style='task', lw=1)
+    ax.set_xticklabels(ax.get_xticklabels(), size=8)
+    ax.set_yticklabels(ax.get_yticklabels(), size=8)
+    ax.set(xlabel='Epoche', ylabel='cls loss')
+    plt.legend(loc='upper right', borderaxespad=0, fontsize=8)
+    plt.title('Vergleich Class-Loss')
+    plt.savefig(r"D:\MAS_DataScience\Test\training_cls_loss_epoch.png", dpi=300)
+    plt.show()
+    plt.cla()
 
-    # adding legend to the curve
-    plt.legend()
+    # Comparison DFL-Loss
+    sns.set_style('whitegrid')
+    ax = sns.lineplot(data=df_model_comp_train_val, x="epoch", y="dfl_loss", hue="model", style='task', lw=1)
+    ax.set_xticklabels(ax.get_xticklabels(), size=8)
+    ax.set_yticklabels(ax.get_yticklabels(), size=8)
+    ax.set(xlabel='Epoche', ylabel='box loss')
+    plt.legend(loc='upper right', borderaxespad=0, fontsize=8)
+    plt.title('Vergleich Box-Loss')
+    plt.savefig(r"D:\MAS_DataScience\Test\training_dfl_loss_epoch.png", dpi=300)
     plt.show()
     plt.cla()
 
@@ -260,7 +218,7 @@ def compare_test_val():
     plt.subplots_adjust(right=0.72, bottom=0.3)
     plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0, fontsize=8)
     plt.title('Vergleich Precision')
-    plt.savefig(r"D:\MAS_DataScience\plots\percision_per_class.png", dpi=300)
+    plt.savefig(r"D:\MAS_DataScience\plots\testval_percision_per_class.png", dpi=300)
     plt.show()
     plt.cla()
 
@@ -274,7 +232,7 @@ def compare_test_val():
     plt.subplots_adjust(right=0.72, bottom=0.3)
     plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0, fontsize=8)
     plt.title('Vergleich Recall')
-    plt.savefig(r"D:\MAS_DataScience\plots\recall_per_class.png", dpi=300)
+    plt.savefig(r"D:\MAS_DataScience\plots\testval_recall_per_class.png", dpi=300)
     plt.show()
     plt.cla()
 
@@ -288,7 +246,7 @@ def compare_test_val():
     plt.subplots_adjust(right=0.72, bottom=0.3)
     plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0, fontsize=8)
     plt.title('Vergleich mAP50')
-    plt.savefig(r"D:\MAS_DataScience\plots\map50_per_class.png", dpi=300)
+    plt.savefig(r"D:\MAS_DataScience\plots\testval_map50_per_class.png", dpi=300)
     plt.show()
     plt.cla()
 
@@ -302,6 +260,6 @@ def compare_test_val():
     plt.subplots_adjust(right=0.72, bottom=0.3)
     plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0, fontsize=8)
     plt.title('Vergleich mAP50-95')
-    plt.savefig(r"D:\MAS_DataScience\plots\map50-95_per_class.png", dpi=300)
+    plt.savefig(r"D:\MAS_DataScience\plots\testval_map50-95_per_class.png", dpi=300)
     plt.show()
     plt.cla()
